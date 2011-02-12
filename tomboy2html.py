6230f8e8454a0072939297cb3e73ed93dac98d1c
Ruslan Osmanov <rrosmanov@gmail.com>
2011-02-12 18:04:37 +0500

    Added: doc & licence

--- tomboy2html.py
+++ tomboy2html.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+# Copyright (C) 2011 - Ruslan Osmanov <rrosmanov@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# See <http://www.gnu.org/licenses/>
+# 
 ##
 # @file tomboy2html.py
 # @brief Export all Tomboy notes in XHTML/HTML file(s)
@@ -6,6 +23,7 @@
 # @version 1.0
 # @date 08.02.2011
 # @details xsltproc utility required
+# @copyright Copyright (C) 2011 - Ruslan Osmanov
 import dbus, gobject, dbus.glib
 import os, sys, getopt
 import re
